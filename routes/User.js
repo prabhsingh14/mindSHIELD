3e80d0c17d76b6e593a24a3ef14a84d81c811f76
Prabh Singh <101253649+prabhsingh14@users.noreply.github.com>
2024-08-29 22:46:57 +0530

    User routes

---
+++ routes/User.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the required controllers and middleware functions
+const {
+    login,
+    signup,
+    sendotp,
+    changePassword,
+} = require("../controllers/Auth")
+const {
+    resetPasswordToken,
+    resetPassword,
+} = require("../controllers/ResetPassword")
+
+const { auth } = require("../middlewares/auth")
+
+router.post("/login", login)
+router.post("/signup", signup)
+router.post("/sendotp", sendotp)
+router.post("/changepassword", auth, changePassword)
+
+router.post("/reset-password-token", resetPasswordToken)
+router.post("/reset-password", resetPassword)
+
+module.exports = router;
