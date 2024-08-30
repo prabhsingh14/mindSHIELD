ba1efee87e1f5eb414159a20ce892b9369e9cf0f
Prabh Singh <101253649+prabhsingh14@users.noreply.github.com>
2024-08-30 22:42:30 +0530

    Delete routes/Collision.js

--- routes/Collision.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {detectCollision} = require("../controllers/CollisionDetection");
-
-router.ws('/collision', detectCollision);
-
-module.exports = router;
